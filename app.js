88e1b9a8fe4f9c4c63986b5e03286c97e0fc737e
JesusChuz <45716413+JesusChuz@users.noreply.github.com>
2021-08-19 10:50:36 -0600

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const index = require('./routes/index');
 
 const app = express();
 
-// uncomment after placing your favicon in /public
+// jello uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
